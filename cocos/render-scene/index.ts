2b4fd61420352ff570f483731b530952453defd4
James Chen <dumganhar@gmail.com>
2025-01-25 12:54:49 +0800

    update render-scene/index.ts

--- cocos/render-scene/index.ts
+++ cocos/render-scene/index.ts
@@ -34,7 +34,6 @@ export { programLib, getDeviceShaderVersion } from './core/program-lib';
 export * from './core/texture-buffer-pool';
 export * from './core/material-instance';
 export * from './core/pass-instance';
-export * from './core/memory-pools';
 export * from './core/render-scene';
 export { RenderWindow } from './core/render-window';
 export * from './deprecated';
