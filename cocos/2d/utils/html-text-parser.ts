a12384aa7ebed2672283fa27196b479b4d3aa014
James Chen <dumganhar@gmail.com>
2025-01-26 14:05:57 +0800

    Don't mangle HTML parser properties.

--- cocos/2d/utils/html-text-parser.ts
+++ cocos/2d/utils/html-text-parser.ts
@@ -36,7 +36,6 @@ const imageAttrReg = /(\s)*src(\s)*=|(\s)*height(\s)*=|(\s)*width(\s)*=|(\s)*ali
  * @en A utils class for parsing HTML texts. The parsed results will be an object array.
  * @zh 一个用于解析HTML文本的工具类。解析后的结果将是一个对象数组。
  * @deprecated since v3.5.0, this is an engine private interface that will be removed in the future.
- * @mangle
  */
 export interface IHtmlTextParserResultObj{
     text?: string;
@@ -47,7 +46,6 @@ export interface IHtmlTextParserResultObj{
  * @en Html Text Parser Stack interface
  * @zh Html 文本解析器接口
  * @deprecated since v3.5.0, this is an engine private interface that will be removed in the future.
- * @mangle
  */
 export interface IHtmlTextParserStack{
     color?: string;
